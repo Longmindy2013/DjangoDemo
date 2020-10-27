e856acb04d34bd4448225f00b3a0fedc904d6154
longmin <1624683462@qq.com>
2020-10-27 17:58:17 +0800

    testplatform init 09

--- testplatform/apitest/models.py
+++ testplatform/apitest/models.py
@@ -3,8 +3,26 @@ from django.db import models
 # Create your models here.
 
 
-class login(models.Model):
-	pass
+class User(models.Model):
+
+	gender = (
+		('male', '男'),
+		('female', '女')
+	)
+
+	name = models.CharField(max_length=16, unique=True)
+	password = models.CharField(max_length=256)
+	email = models.EmailField(unique=True)
+	sex = models.CharField(max_length=16, choices=gender, default='男')
+	ctime = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ["-ctime"]
+		verbose_name = '用户'
+		verbose_name_plural = '用户'
 
 
 class tucao(models.Model):
@@ -14,3 +32,11 @@ class tucao(models.Model):
 
 	def __str__(self):
 		return self.text + str(self.ctime)
+
+
+class home_link(models.Model):
+	link_name = models.CharField(max_length=50, null=True, help_text='超链接名称')
+	link_content = models.CharField(max_length=1024, null=True, help_text='超链接内容')
+
+	def __str__(self):
+		return self.link_name
